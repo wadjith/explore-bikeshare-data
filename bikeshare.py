11afc0b951c6560173054bdefe536e0600c8d65e
Thierry WADJI <wadjith@yahoo.fr>
2020-01-25 16:04:43 +0100

    Correct spelling mistakes on screen messages

--- bikeshare.py
+++ bikeshare.py
@@ -15,7 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US Bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     cities = ['chicago', 'new york city', 'washington']
     while True:
@@ -94,7 +94,7 @@ def load_data(city, month, day):
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
-    print('\nCalculating The Most Frequent Times of Travel...\n')
+    print('\nCalculating The most frequent times of Travel...\n')
     startTime = time.time()
 
     # display the most common month
